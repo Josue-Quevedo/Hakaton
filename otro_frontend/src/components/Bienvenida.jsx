6c8eb4d15b5eeca9798973ac7e7f55e94c2f0d13
AngelOsmany <160093071+AngelOsmany@users.noreply.github.com>
2024-04-06 02:09:48 -0600

    canisters

---
+++ otro_frontend/src/components/Bienvenida.jsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  
+  return (
+    <section className="mt-5 text-center">
+      <h1>Bienvenido a Quality Earth</h1>
+      
+      
+    </section>
+  )
+}
+
+
+export default Login
